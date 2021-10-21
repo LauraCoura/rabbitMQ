f34c8401d3d77a9bb3f7791051c6c14d2e90c891
Laura <58921778+LauraCoura@users.noreply.github.com>
2021-10-21 00:18:33 -0300

    Update emitter.js

--- rotas/emitter.js
+++ rotas/emitter.js
@@ -42,7 +42,7 @@ const sendMsg = async () => {
 
     arrayMsg.forEach(msg => {
       channel.publish(exchangeName, msg.screenName, Buffer.from(msg.text));
-      console.log("\x1b[0.42;0.68;0.87;255;255m", 'Sent: ', msg.text);
+      console.log("\x1b[33m%s\x1b[0m", 'Sent: ', msg.text);
     });
 
     setTimeout(() => {
